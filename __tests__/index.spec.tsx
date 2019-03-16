cd98785af6792e4c4c08bc74e28bb68581f01a34
ble <ble.message@gmail.com>
2019-03-16 13:41:46 -0400

    added jest and enzyme

---
+++ __tests__/index.spec.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { mount } from 'enzyme';
+import IndexPage from '../pages/index';
+
+describe('Test description', () => {
+  describe('Specific test', () => {
+    it('test expectation', function() {
+      // write tests here
+    });
+  });
+});
